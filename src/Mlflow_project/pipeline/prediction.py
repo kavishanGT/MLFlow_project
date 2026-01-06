fdc0ddad9a91ce25aba12257a05067b8fd0286a1
KavishanGT <kavishangt.21@uom.lk>
2026-01-06 19:13:11 +0530

    model prediction  and api code added

---
+++ src/Mlflow_project/pipeline/prediction.py
@@ -0,0 +1,38 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import os
+
+
+
+class PredictionPipeline:
+    def __init__(self,filename):
+        self.filename =filename
+
+
+    
+    def predict(self):
+        ## load model
+        
+        # model = load_model(os.path.join("artifacts","training", "model.h5"))
+        model = load_model(os.path.join("resources","model_trainer", "trained_model.h5"))
+
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (224,224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = np.argmax(model.predict(test_image), axis=1)
+        print(result)
+
+        if result[0] == 3:
+            prediction = 'pitiuitary Tumor'
+            return [{ "image" : prediction}]
+        elif result[0] == 2:
+            prediction = 'notumor'
+            return [{ "image" : prediction}]
+        elif result[0] == 1:
+            prediction = 'meningioma Tumor'
+            return [{ "image" : prediction}]
+        else:
+            prediction = 'glioma Tumor'
+            return [{ "image" : prediction}]
